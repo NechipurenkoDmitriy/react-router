bee0fee59af8e266ba2d24e71c17c3f2b2d7fcde
NechipurenkoDmitriy <pikinaplecho@gmail.com>
2022-08-11 16:19:05 +0300

    First commit

---
+++ src/app/components/not-found.jsx
@@ -0,0 +1,5 @@
+const NotFond = () => {
+	return <h1>404 NotFond</h1>;
+};
+
+export default NotFond;
---
+++ src/app/components/posts.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Post from './post';
+import PostsList from './postsList';
+// import query from 'query-string';
+// import _ from 'lodash';
+import {useParams} from 'react-router-dom';
+
+const Posts = () => {
+	const params = useParams();
+	const posts = [
+		{id: 1, label: 'post 1'},
+		{id: 2, label: 'post 2'},
+		{id: 3, label: 'post 3'},
+	];
+
+	const {postId} = params;
+
+	return (
+		<>
+			{postId ? (
+				<>
+					<Post id={postId} posts={posts} />
+				</>
+			) : (
+				<PostsList posts={posts} />
+			)}
+		</>
+	);
+};
+
+export default Posts;
